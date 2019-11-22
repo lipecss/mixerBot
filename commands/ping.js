8be1f139bf08613f0a70b7da7260b96a411ecc1b
Felipecss <felipecss@vemlavaralouca.com.br>
2019-11-22 17:47:08 -0300

    atualizando os pacotes

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const Mixer = require('@mixer/client-node');
 const ping = require('node-http-ping');
 
-exports.run = async (client, data, args, userId, channelId, socket) => {
+exports.run = async (data, args, userId, channelId, socket, msg) => {
     const str = String(new Date().getMilliseconds()).padStart(3, '0');
     ping('https://status.mixer.com')
     .then((time) => {
